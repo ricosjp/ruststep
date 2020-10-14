ab0c8c67510cdf4e91cd9903af0fd30a2873bd17
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-10-15 01:11:38 +0900

    Minor format

--- exp2rs/src/lib.rs
+++ exp2rs/src/lib.rs
@@ -1,7 +1,7 @@
-extern crate iso_10303_simple_types;
 extern crate inflector;
-pub use iso_10303_simple_types::*;
+extern crate iso_10303_simple_types;
 use inflector::Inflector;
-pub mod types;
+pub use iso_10303_simple_types::*;
 pub mod decode;
+pub mod types;
 pub use decode::decode;
