d7d14d038f3dac743efb9c2be9360bd0c4ff7c32
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-11-06 21:45:26 +0900

    Move code generations target into generated/

--- exp2rs/tests/decode.rs
+++ exp2rs/tests/decode.rs
@@ -1,12 +1,15 @@
-extern crate exp2rs;
-use std::io::Write;
+use std::{env, fs, io::Write, path::*};
 
 #[test]
 fn decode() {
     let code = include_str!("test.exp");
     let schemas = exp2rs::decode(code).unwrap();
+    let out_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("generated");
+    if !out_dir.exists() {
+        fs::create_dir_all(&out_dir).unwrap();
+    }
 
-    let mut file = std::fs::File::create("tests/decoded.rs").unwrap();
+    let mut file = std::fs::File::create(out_dir.join("decoded.rs")).unwrap();
     file.write(b"#![allow(dead_code)]\n").unwrap();
     for schema in schemas {
         file.write(schema.rust_code().as_bytes()).unwrap();
