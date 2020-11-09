f628086ab2e9131c8f0b2a9b8a6091d5a1065df4
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-11-09 13:31:17 +0900

    Explicit attribute

--- exp2rs/src/parser/entity.rs
+++ exp2rs/src/parser/entity.rs
@@ -20,24 +20,42 @@ pub struct Entity {
     pub attributes: Vec<(String, String)>,
 }
 
-fn entity_line(input: &str) -> IResult<&str, (String, String)> {
-    todo!()
+/// 9.2.1.1 Explicit attribute
+///
+/// ```text
+/// 215 explicit_attr = attribute_decl { ’,’ attribute_decl } ’:’ [ OPTIONAL ] parameter_type ’;’ .
+/// 177 attribute_decl = attribute_id | redeclared_attribute .
+/// 266 parameter_type = generalized_types | named_types | simple_types .
+/// ```
+fn explicit_attr(input: &str) -> IResult<&str, (Vec<String>, String)> {
+    tuple((
+        separated_list0(tuple((space0, tag(","), space0)), identifier),
+        space0,
+        tag(":"),
+        space0,
+        identifier, // FIXME this must be type instead of identifier
+        space0,
+        tag(";"),
+    ))
+    .map(|(attrs, _, _, _, ty, _, _)| (attrs, ty))
+    .parse(input)
 }
 
-fn entity_header(input: &str) -> IResult<&str, String> {
+fn entity_head(input: &str) -> IResult<&str, String> {
     tuple((tag("ENTITY"), space1, identifier, space0, tag(";")))
         .map(|(_, _, id, _, _)| id)
         .parse(input)
 }
 
+/// 9.2 Entity declaration
+///
+/// ```text
+/// 206 entity_decl = entity_head entity_body END_ENTITY ’;’ .
+/// 207 entity_head = ENTITY entity_id subsuper ’;’ .
+/// 204 entity_body = { explicit_attr } [ derive_clause ] [ inverse_clause ] [ unique_clause ] [ where_clause ] .
+/// ```
 pub fn entity(input: &str) -> IResult<&str, Entity> {
     todo!()
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Schema {
-    pub name: String,
-    pub entities: Vec<Entity>,
 }
 
 #[cfg(test)]
@@ -45,9 +63,22 @@ mod tests {
     use nom::Finish;
 
     #[test]
-    fn entity_header() {
-        let (residual, name) = super::entity_header("ENTITY homhom;").finish().unwrap();
+    fn entity_head() {
+        let (residual, name) = super::entity_head("ENTITY homhom;").finish().unwrap();
         assert_eq!(name, "homhom");
+        assert_eq!(residual, "");
+    }
+
+    #[test]
+    fn explicit_attr() {
+        let (residual, (id, ty)) = super::explicit_attr("x : Real;").finish().unwrap();
+        assert_eq!(id, &["x"]);
+        assert_eq!(ty, "Real");
+        assert_eq!(residual, "");
+
+        let (residual, (id, ty)) = super::explicit_attr("x, y : Real;").finish().unwrap();
+        assert_eq!(id, &["x", "y"]);
+        assert_eq!(ty, "Real");
         assert_eq!(residual, "");
     }
 }
