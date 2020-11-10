7f27f0e0368e42334b4d54235754999fcc7cc333
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-11-10 17:40:21 +0900

    Operators

--- exp2rs/src/parser/expr.rs
+++ exp2rs/src/parser/expr.rs
@@ -1,4 +1,4 @@
-use nom::IResult;
+use nom::{branch::*, bytes::complete::*, combinator::*, IResult, Parser};
 
 pub struct Expr {}
 
@@ -15,6 +15,125 @@ pub struct Expr {}
 /// 257 multiplication_like_op = ’*’ | ’/’ | DIV | MOD | AND | ’||’ .
 /// 168 add_like_op = ’+’ | ’-’ | OR | XOR .
 /// ```
-pub fn expr(input: &str) -> IResult<&str, Expr> {
+pub fn expr(_input: &str) -> IResult<&str, Expr> {
     todo!()
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum RelOp {
+    /// "="
+    Equal,
+    /// "<>"
+    NotEqual,
+    /// "<"
+    LT,
+    /// ">"
+    GT,
+    /// "<="
+    LEQ,
+    /// ">="
+    GEQ,
+    /// ":=:"
+    InstanceEqual,
+    /// ":<>:"
+    InstanceNotEqual,
+}
+
+/// ```text
+/// 331 unary_op = ’+’ | ’-’ | NOT .
+/// ```
+pub fn rel_op(input: &str) -> IResult<&str, RelOp> {
+    use RelOp::*;
+    alt((
+        value(Equal, tag("=")),
+        value(NotEqual, tag("<>")),
+        value(LT, tag("<")),
+        value(GT, tag(">")),
+        value(LEQ, tag("<=")),
+        value(GEQ, tag(">=")),
+        value(InstanceEqual, tag(":=:")),
+        value(InstanceNotEqual, tag(":<>:")),
+    ))
+    .parse(input)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum UnaryOp {
+    /// "+"
+    Plus,
+    /// "-"
+    Minus,
+    /// "NOT"
+    Not,
+}
+
+/// ```text
+/// 331 unary_op = ’+’ | ’-’ | NOT .
+/// ```
+pub fn unary_op(input: &str) -> IResult<&str, UnaryOp> {
+    use UnaryOp::*;
+    alt((
+        value(Plus, tag("+")),
+        value(Minus, tag("-")),
+        value(Not, tag("NOT")),
+    ))
+    .parse(input)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum MultiplicationLikeOp {
+    /// "*"
+    Mul,
+    /// "/"
+    RealDiv,
+    /// "DIV"
+    IntegerDiv,
+    /// "MOD"
+    Mod,
+    /// "AND"
+    And,
+    /// "||", Complex entity instance construction operator (12.10)
+    ComplexEntityInstanceConstruction,
+}
+
+/// ```text
+/// 257 multiplication_like_op = ’*’ | ’/’ | DIV | MOD | AND | ’||’ .
+/// ```
+pub fn multiplication_like_op(input: &str) -> IResult<&str, MultiplicationLikeOp> {
+    use MultiplicationLikeOp::*;
+    alt((
+        value(Mul, tag("*")),
+        value(RealDiv, tag("/")),
+        value(IntegerDiv, tag("DIV")),
+        value(Mod, tag("MOD")),
+        value(And, tag("AND")),
+        value(ComplexEntityInstanceConstruction, tag("||")),
+    ))
+    .parse(input)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum AddLikeOp {
+    /// "+"
+    Add,
+    /// "-"
+    Sub,
+    /// "OR"
+    Or,
+    /// "XOR"
+    Xor,
+}
+
+/// ```text
+/// 168 add_like_op = ’+’ | ’-’ | OR | XOR .
+/// ```
+pub fn add_like_op(input: &str) -> IResult<&str, AddLikeOp> {
+    use AddLikeOp::*;
+    alt((
+        value(Add, tag("+")),
+        value(Sub, tag("-")),
+        value(Or, tag("OR")),
+        value(Xor, tag("XOR")),
+    ))
+    .parse(input)
 }
