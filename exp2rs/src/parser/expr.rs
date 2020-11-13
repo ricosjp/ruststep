9aba2761bdbc3f37f02ae99ef300007964e06d90
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-11-13 21:27:50 +0900

    Expr

--- exp2rs/src/parser/expr.rs
+++ exp2rs/src/parser/expr.rs
@@ -2,21 +2,80 @@ use super::*;
 use derive_more::From;
 use nom::{branch::*, bytes::complete::*, combinator::*, IResult, Parser};
 
-pub struct Expr {}
-
-/// ```text
-/// 216 expression = simple_expression [ rel_op_extended simple_expression ] .
-/// 305 simple_expression = term { add_like_op term } .
-/// 325 term = factor { multiplication_like_op factor } .
-/// 217 factor = simple_factor [ `**` simple_factor ] .
+/// Unary expresion, e.g. `x` or binary expression `x + y`
+///
+/// Example
+/// --------
+///
 /// ```
-pub fn expr(_input: &str) -> IResult<&str, Expr> {
-    todo!()
+/// use exp2rs::parser::*;
+/// use nom::Finish;
+///
+/// let (residual, e) = expression("1 - 2").finish().unwrap();
+/// assert_eq!(residual, "");
+/// ```
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr<Base, Op> {
+    Unary(Base),
+    Binary { op: Op, arg1: Base, arg2: Base },
 }
 
-#[derive(Debug, Clone, PartialEq, From)]
+impl<Base, Op> From<Base> for Expr<Base, Op> {
+    fn from(base: Base) -> Self {
+        Expr::Unary(base)
+    }
+}
+
+pub type Expression = Expr<SimpleExpression, RelOpExtended>;
+pub type SimpleExpression = Expr<Term, AddLikeOp>;
+pub type Term = Expr<Factor, MultiplicationLikeOp>;
+pub type Factor = Expr<SimpleFactor, PowerOp>;
+
+fn expr<Base, Op>(
+    input: &str,
+    base_parse: impl Fn(&str) -> IResult<&str, Base> + Copy,
+    op_parser: impl Fn(&str) -> IResult<&str, Op>,
+) -> IResult<&str, Expr<Base, Op>> {
+    tuple((
+        base_parse,
+        opt(tuple((multispace0, op_parser, multispace0, base_parse))),
+    ))
+    .map(|(base, opt)| {
+        if let Some((_, op, _, arg2)) = opt {
+            Expr::Binary {
+                op,
+                arg1: base,
+                arg2,
+            }
+        } else {
+            Expr::Unary(base)
+        }
+    })
+    .parse(input)
+}
+
+/// 216 expression = simple_expression [ rel_op_extended simple_expression ] .
+pub fn expression(input: &str) -> IResult<&str, Expression> {
+    expr(input, simple_expression, rel_op_extended)
+}
+
+/// 305 simple_expression = term { add_like_op term } .
+pub fn simple_expression(input: &str) -> IResult<&str, SimpleExpression> {
+    expr(input, term, add_like_op)
+}
+
+/// 325 term = factor { multiplication_like_op factor } .
+pub fn term(input: &str) -> IResult<&str, Term> {
+    expr(input, factor, multiplication_like_op)
+}
+
+/// 217 factor = simple_factor [ `**` simple_factor ] .
+pub fn factor(input: &str) -> IResult<&str, Factor> {
+    expr(input, simple_factor, power_op)
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum SimpleFactor {
-    #[from(ignore)]
     Primary {
         unary_op: Option<UnaryOp>,
         primary: Primary,
@@ -226,4 +285,17 @@ pub fn add_like_op(input: &str) -> IResult<&str, AddLikeOp> {
         value(Xor, tag("XOR")),
     ))
     .parse(input)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum PowerOp {
+    /// `**`
+    Power,
+}
+
+/// 999 power_op = `**`
+///
+/// Additional trivial rule for managing operators uniformly
+pub fn power_op(input: &str) -> IResult<&str, PowerOp> {
+    value(PowerOp::Power, tag("**")).parse(input)
 }
