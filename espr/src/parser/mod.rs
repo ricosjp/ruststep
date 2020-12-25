157da161d27464f426afe928a2bd448d2a1486a7
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-12-26 00:07:05 +0900

    Rename output of parser as "SyntaxTree"

--- espr/src/parser/mod.rs
+++ espr/src/parser/mod.rs
@@ -20,13 +20,13 @@ use nom::{
     Parser,
 };
 
-/// Entrypoint of entire EXPRESS language parser
+/// Entire syntax tree parsed from EXPRESS Language string
 ///
 /// Example
 /// --------
 ///
 /// ```
-/// let schemas = espr::parser::parse(r#"
+/// let schemas = espr::parser::SyntaxTree::parse(r#"
 /// SCHEMA one;
 ///   ENTITY first;
 ///     m_ref : second;
@@ -44,17 +44,53 @@ use nom::{
 /// END_SCHEMA;
 /// "#.trim()).unwrap();
 /// ```
-pub fn parse(input: &str) -> Result<Vec<Schema>, nom::error::Error<&str>> {
-    let (_residual, schemas) = tuple((
-        multispace0,
-        separated_list1(multispace1, schema),
-        multispace0,
-    ))
-    .map(|(_, schemas, _)| schemas)
-    .parse(input)
-    .finish()?;
-    // should check residual here
-    Ok(schemas)
+#[derive(Debug, Clone, PartialEq)]
+pub struct SyntaxTree {
+    pub schemas: Vec<Schema>,
+}
+
+impl SyntaxTree {
+    pub fn parse(input: &str) -> Result<Self, nom::error::Error<&str>> {
+        let (_residual, schemas) = tuple((
+            multispace0,
+            separated_list1(multispace1, schema),
+            multispace0,
+        ))
+        .map(|(_, schemas, _)| schemas)
+        .parse(input)
+        .finish()?;
+        // FIXME should check residual here
+        Ok(Self { schemas })
+    }
+
+    // Example syntax tree for easy testing
+    //
+    // FIXME Replace by e.g. proptest
+    // https://github.com/AltSysrq/proptest
+    #[allow(dead_code)]
+    pub(crate) fn example() -> Self {
+        Self::parse(
+            r#"
+            SCHEMA one;
+              ENTITY first;
+                m_ref : second;
+                fattr : STRING;
+              END_ENTITY;
+              ENTITY second;
+                sattr : STRING;
+              END_ENTITY;
+            END_SCHEMA;
+
+            SCHEMA geometry0;
+              ENTITY point;
+                x, y, z: REAL;
+              END_ENTITY;
+            END_SCHEMA;
+            "#
+            .trim(),
+        )
+        .unwrap()
+    }
 }
 
 /// 128 letter = `a` | `b` | `c` | `d` | `e` | `f` | `g` | `h` | `i` | `j` | `k` | `l` |`m` | `n` | `o` | `p` | `q` | `r` | `s` | `t` | `u` | `v` | `w` | `x` |`y` | `z` .
