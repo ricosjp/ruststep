d9782fe3a928f4129e4d4817d1ccf4d28ef3dc44
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-12-26 00:33:45 +0900

    moc IR struct

--- espr/src/semantics/mod.rs
+++ espr/src/semantics/mod.rs
@@ -1,5 +1,18 @@
+//! Legalize [SyntaxTree] into [IR]
+
 mod namespace;
 mod scope;
 
 pub use namespace::*;
 pub use scope::*;
+
+use crate::parser::SyntaxTree;
+
+/// Intermediate Representation
+pub struct IR {}
+
+impl IR {
+    pub fn legalize(_syn: SyntaxTree) -> Result<Self, ()> {
+        todo!()
+    }
+}
