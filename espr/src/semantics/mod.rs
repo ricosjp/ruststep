1958c4e435562c8d0638f29fae95ae552eb50076
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-01-10 19:37:07 +0900

    esprc

--- espr/src/semantics/mod.rs
+++ espr/src/semantics/mod.rs
@@ -12,6 +12,7 @@ pub use scope::Scope;
 use crate::parser::syntax_tree::SyntaxTree;
 use proc_macro2::TokenStream;
 use quote::*;
+use std::fmt;
 use thiserror::Error;
 
 /// Semantic errors
@@ -35,6 +36,24 @@ pub trait Legalize: Sized {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct IR {
     pub schemas: Vec<Schema>,
+}
+
+impl IR {
+    pub fn from_syntax_tree(st: &SyntaxTree) -> Result<Self, SemanticError> {
+        let ns = Namespace::new(&st)?;
+        let ir = Self::legalize(&ns, &Scope::root(), &st)?;
+        Ok(ir)
+    }
+}
+
+impl fmt::Display for IR {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "#![allow(dead_code)]\n{}",
+            self.to_token_stream().to_string()
+        )
+    }
 }
 
 impl Legalize for IR {
