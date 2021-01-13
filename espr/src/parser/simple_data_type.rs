a229cede612ecac43cbc1a1d489820302cd49074
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-01-13 16:41:05 +0900

    Use parser::util in simple_data_type

--- espr/src/parser/simple_data_type.rs
+++ espr/src/parser/simple_data_type.rs
@@ -1,7 +1,4 @@
-use nom::{
-    branch::alt, bytes::complete::*, character::complete::*, combinator::*, sequence::*, IResult,
-    Parser,
-};
+use super::util::*;
 
 /// Parsed result of `width_spec`
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -30,7 +27,7 @@ pub enum SimpleType {
 }
 
 /// 307 simple_types = binary_type | boolean_type | integer_type | logical_type | number_type | real_type | string_type .
-pub fn simple_types(input: &str) -> IResult<&str, SimpleType> {
+pub fn simple_types(input: &str) -> ParseResult<SimpleType> {
     alt((
         number_type,
         real_type,
@@ -44,63 +41,59 @@ pub fn simple_types(input: &str) -> IResult<&str, SimpleType> {
 }
 
 /// 261 number_type = NUMBER .
-pub fn number_type(input: &str) -> IResult<&str, SimpleType> {
+pub fn number_type(input: &str) -> ParseResult<SimpleType> {
     value(SimpleType::Number, tag("NUMBER")).parse(input)
 }
 
 /// 278 real_type = REAL \[ `(` precision_spec `)` \] .
 ///
 /// 268 precision_spec = numeric_expression .
-pub fn real_type(input: &str) -> IResult<&str, SimpleType> {
+pub fn real_type(input: &str) -> ParseResult<SimpleType> {
     // FIXME precision_spec is not supported
     value(SimpleType::Real, tag("REAL")).parse(input)
 }
 
 /// 241 integer_type = INTEGER .
-pub fn integer_type(input: &str) -> IResult<&str, SimpleType> {
+pub fn integer_type(input: &str) -> ParseResult<SimpleType> {
     value(SimpleType::Integer, tag("INTEGER")).parse(input)
 }
 
 /// 256 logical_type = LOGICAL .
-pub fn logical_type(input: &str) -> IResult<&str, SimpleType> {
+pub fn logical_type(input: &str) -> ParseResult<SimpleType> {
     value(SimpleType::Logical, tag("LOGICAL")).parse(input)
 }
 
 /// 182 boolean_type = BOOLEAN .
-pub fn boolen_type(input: &str) -> IResult<&str, SimpleType> {
+pub fn boolen_type(input: &str) -> ParseResult<SimpleType> {
     value(SimpleType::Boolen, tag("BOOLEN")).parse(input)
 }
 
 /// 311 string_type = STRING \[ width_spec \] .
-pub fn string_type(input: &str) -> IResult<&str, SimpleType> {
-    tuple((tag("STRING"), multispace0, opt(width_spec)))
-        .map(|(_, _, width_spec)| SimpleType::String_ { width_spec })
+pub fn string_type(input: &str) -> ParseResult<SimpleType> {
+    tuple((tag("STRING"), opt(width_spec)))
+        .map(|(_, width_spec)| SimpleType::String_ { width_spec })
         .parse(input)
 }
 
 /// 181 binary_type = BINARY \[ width_spec \] .
-pub fn binary_type(input: &str) -> IResult<&str, SimpleType> {
-    tuple((tag("BINARY"), multispace0, opt(width_spec)))
-        .map(|(_, _, width_spec)| SimpleType::Binary { width_spec })
+pub fn binary_type(input: &str) -> ParseResult<SimpleType> {
+    tuple((tag("BINARY"), opt(width_spec)))
+        .map(|(_, width_spec)| SimpleType::Binary { width_spec })
         .parse(input)
 }
 
 /// 341 width_spec = `(` width `)` \[ FIXED \] .
-pub fn width_spec(input: &str) -> IResult<&str, WidthSpec> {
+pub fn width_spec(input: &str) -> ParseResult<WidthSpec> {
     // FIXME Should use `numeric_expression` parser
-    tuple((
-        delimited(char('('), is_not(")"), char(')')),
-        multispace0,
-        opt(tag("FIXED")),
-    ))
-    .map(|(width, _, fixed): (&str, _, _)| {
-        let width = width.parse::<usize>().unwrap(); // FIXME should raise error instead of panic
-        WidthSpec {
-            width,
-            fixed: fixed.is_some(),
-        }
-    })
-    .parse(input)
+    tuple((char('('), is_not(")"), char(')'), opt(tag("FIXED"))))
+        .map(|(_lparen, width, _rparen, fixed)| {
+            let width = width.parse::<usize>().unwrap(); // FIXME should raise error instead of panic
+            WidthSpec {
+                width,
+                fixed: fixed.is_some(),
+            }
+        })
+        .parse(input)
 }
 
 #[cfg(test)]
@@ -110,11 +103,11 @@ mod tests {
 
     #[test]
     fn string() {
-        let (res, string) = super::string_type("STRING").finish().unwrap();
+        let (res, (string, _remarks)) = super::string_type("STRING").finish().unwrap();
         assert_eq!(string, SimpleType::String_ { width_spec: None });
         assert_eq!(res, "");
 
-        let (res, string) = super::string_type("STRING (10)").finish().unwrap();
+        let (res, (string, _remarks)) = super::string_type("STRING (10)").finish().unwrap();
         assert_eq!(
             string,
             SimpleType::String_ {
@@ -126,7 +119,7 @@ mod tests {
         );
         assert_eq!(res, "");
 
-        let (res, string) = super::string_type("STRING (10) FIXED").finish().unwrap();
+        let (res, (string, _remarks)) = super::string_type("STRING (10) FIXED").finish().unwrap();
         assert_eq!(
             string,
             SimpleType::String_ {
@@ -141,11 +134,11 @@ mod tests {
 
     #[test]
     fn binary() {
-        let (res, binary) = super::binary_type("BINARY").finish().unwrap();
+        let (res, (binary, _remarks)) = super::binary_type("BINARY").finish().unwrap();
         assert_eq!(binary, SimpleType::Binary { width_spec: None });
         assert_eq!(res, "");
 
-        let (res, binary) = super::binary_type("BINARY (10)").finish().unwrap();
+        let (res, (binary, _remarks)) = super::binary_type("BINARY (10)").finish().unwrap();
         assert_eq!(
             binary,
             SimpleType::Binary {
@@ -157,7 +150,7 @@ mod tests {
         );
         assert_eq!(res, "");
 
-        let (res, binary) = super::binary_type("BINARY (10) FIXED").finish().unwrap();
+        let (res, (binary, _remarks)) = super::binary_type("BINARY (10) FIXED").finish().unwrap();
         assert_eq!(
             binary,
             SimpleType::Binary {
