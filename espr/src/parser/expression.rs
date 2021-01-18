c9521e490b27dc7042baa65e8da61282ad0b4543
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-01-19 04:48:23 +0900

    qualifiers

--- espr/src/parser/expression.rs
+++ espr/src/parser/expression.rs
@@ -1,4 +1,4 @@
-use super::{literal::*, util::*};
+use super::{basis::*, literal::*, util::*};
 use derive_more::From;
 
 /// Unary expresion, e.g. `x` or binary expression `x + y`
@@ -139,6 +139,56 @@ pub fn primary(input: &str) -> ParseResult<Primary> {
     literal
         .map(|literal| Primary::Literal(literal))
         .parse(input)
+}
+
+pub enum QualifiableFactor {}
+
+/// 274 qualifiable_factor = attribute_ref | constant_factor | function_call | general_ref | population .
+pub fn qualifiable_factor(input: &str) -> ParseResult<QualifiableFactor> {
+    todo!()
+}
+
+pub enum Qualifier {
+    Attribute(String),
+    Group(String),
+    Index {
+        index_1: SimpleExpression,
+        index_2: Option<SimpleExpression>,
+    },
+}
+
+/// 276 qualifier = attribute_qualifier | group_qualifier | index_qualifier .
+pub fn qualifier(input: &str) -> ParseResult<Qualifier> {
+    alt((attribute_qualifier, group_qualifier, index_qualifier)).parse(input)
+}
+
+/// 179 attribute_qualifier = `.` attribute_ref .
+pub fn attribute_qualifier(input: &str) -> ParseResult<Qualifier> {
+    tuple((char('.'), remarked(simple_id)))
+        .map(|(_dot, id)| Qualifier::Attribute(id))
+        .parse(input)
+}
+
+/// 232 group_qualifier = `\` entity_ref .
+pub fn group_qualifier(input: &str) -> ParseResult<Qualifier> {
+    tuple((char('\\'), remarked(simple_id)))
+        .map(|(_dot, id)| Qualifier::Group(id))
+        .parse(input)
+}
+
+/// 239 index_qualifier = `[` index_1 [ `:` index_2 ] `]` .
+pub fn index_qualifier(input: &str) -> ParseResult<Qualifier> {
+    tuple((
+        char('['),
+        simple_expression,
+        opt(tuple((char(':'), simple_expression))),
+        char(']'),
+    ))
+    .map(|(_open, index_1, opt, _close)| Qualifier::Index {
+        index_1,
+        index_2: opt.map(|(_coron, expr)| expr),
+    })
+    .parse(input)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, From)]
