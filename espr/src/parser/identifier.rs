d98748ff4e5f0e3acdf0f91677fe5aca78ebc7b0
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-01-21 23:24:37 +0900

    Define identifiers

---
+++ espr/src/parser/identifier.rs
@@ -0,0 +1,39 @@
+use super::{basis::*, util::*};
+
+macro_rules! impl_id {
+    ($name:ident) => {
+        pub fn $name(input: &str) -> ParseResult<String> {
+            remarked(simple_id).parse(input)
+        }
+    };
+}
+
+impl_id!(attribute_id);
+impl_id!(constant_id);
+impl_id!(entity_id);
+impl_id!(enumeration_id);
+impl_id!(function_id);
+impl_id!(parameter_id);
+impl_id!(procedure_id);
+impl_id!(rule_label_id);
+impl_id!(rule_id);
+impl_id!(schema_id);
+impl_id!(subtype_constraint_id);
+impl_id!(type_label_id);
+impl_id!(type_id);
+impl_id!(variable_id);
+
+impl_id!(attribute_ref);
+impl_id!(constant_ref);
+impl_id!(entity_ref);
+impl_id!(enumeration_ref);
+impl_id!(function_ref);
+impl_id!(parameter_ref);
+impl_id!(procedure_ref);
+impl_id!(rule_label_ref);
+impl_id!(rule_ref);
+impl_id!(schema_ref);
+impl_id!(subtype_constraint_ref);
+impl_id!(type_label_ref);
+impl_id!(type_ref);
+impl_id!(variable_ref);
