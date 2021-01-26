30ebf4b9dc20e49d000561efe371faa97f3331de
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-01-26 22:02:17 +0900

    num_lines for esprc

--- espr/src/bin/esprc.rs
+++ espr/src/bin/esprc.rs
@@ -6,6 +6,8 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Arguments {
+    #[structopt(short = "l", long = "num_lines", default_value = "3")]
+    num_lines: usize,
     #[structopt(parse(from_os_str))]
     source: PathBuf,
 }
@@ -21,7 +23,7 @@ fn main() {
                     "Syntax Error occurred while parsing following line [{:?}]:",
                     kind
                 );
-                for line in code.lines().take(3) {
+                for line in code.lines().take(args.num_lines) {
                     eprintln!("> {}", line);
                 }
                 eprintln!();
