810769cd14626e3e5a1ebf04f36d30f82e18f56e
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-01-30 00:32:17 +0900

    Move doctest into module test

--- espr/src/parser/literal.rs
+++ espr/src/parser/literal.rs
@@ -16,19 +16,8 @@ pub enum Literal {
 
 /// 251 literal = binary_literal | [logical_literal] | [real_literal] | string_literal .
 ///
-/// Integer value, e.g. `23` will be recognized as a real number `23.0`
-///
-/// Example
-/// --------
-///
-/// ```
-/// use espr::parser::literal::*;
-/// use nom::Finish;
-///
-/// let (residual, (l, _remarks)) = literal("23").finish().unwrap();
-/// assert_eq!(l, Literal::Real(23.0));
-/// assert_eq!(residual, "");
-/// ```
+/// Integer value, e.g. `23`, will be recognized as a real number `23.0`.
+/// Use [integer_literal] if you wish to parse it as an integer.
 pub fn literal(input: &str) -> ParseResult<Literal> {
     alt((
         logical_literal.map(|val| Literal::Logial(val)),
@@ -53,18 +42,6 @@ pub fn logical_literal(input: &str) -> ParseResult<Logical> {
 ///
 /// Negative integer, e.g. `-23`,
 /// will be represented by the combination of `-` unary operator and integer literal `23`
-///
-/// Example
-/// --------
-///
-/// ```
-/// use espr::parser::literal::*;
-/// use nom::Finish;
-///
-/// let (residual, (value, _remarks)) = integer_literal("123").finish().unwrap();
-/// assert_eq!(value, 123);
-/// assert_eq!(residual, "");
-/// ```
 pub fn integer_literal(input: &str) -> ParseResult<u64> {
     remarked(nom::character::complete::digit1)
         .map(|d: &str| d.parse().unwrap())
@@ -72,26 +49,33 @@ pub fn integer_literal(input: &str) -> ParseResult<u64> {
 }
 
 /// 142 real_literal = integer_literal | ( digits `.` \[ digits \] \[ `e` \[ sign \] digits \] ) .
-///
-/// Example
-/// --------
-///
-/// ```
-/// use espr::parser::literal::*;
-/// use nom::Finish;
-///
-/// let (residual, (value, _remarks)) = real_literal("123").finish().unwrap();
-/// assert_eq!(value, 123.0);
-/// assert_eq!(residual, "");
-///
-/// let (residual, (value, _remarks)) = real_literal("123.456").finish().unwrap();
-/// assert_eq!(value, 123.456);
-/// assert_eq!(residual, "");
-///
-/// let (residual, (value, _remarks)) = real_literal("1.23e-5").finish().unwrap();
-/// assert_eq!(value, 1.23e-5);
-/// assert_eq!(residual, "");
-/// ```
 pub fn real_literal(input: &str) -> ParseResult<f64> {
     remarked(nom::number::complete::double).parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use nom::Finish;
+
+    #[test]
+    fn integer_literal() {
+        let (residual, (value, _remarks)) = super::integer_literal("123").finish().unwrap();
+        assert_eq!(value, 123);
+        assert_eq!(residual, "");
+    }
+
+    #[test]
+    fn real_literal() {
+        let (residual, (value, _remarks)) = super::real_literal("123").finish().unwrap();
+        assert_eq!(value, 123.0);
+        assert_eq!(residual, "");
+
+        let (residual, (value, _remarks)) = super::real_literal("123.456").finish().unwrap();
+        assert_eq!(value, 123.456);
+        assert_eq!(residual, "");
+
+        let (residual, (value, _remarks)) = super::real_literal("1.23e-5").finish().unwrap();
+        assert_eq!(value, 1.23e-5);
+        assert_eq!(residual, "");
+    }
 }
