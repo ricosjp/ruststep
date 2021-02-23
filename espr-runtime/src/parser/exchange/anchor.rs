c7e170279947f091184582f97e03b4b61fb31e97
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-02-24 04:45:45 +0900

    anchor_section

--- espr-runtime/src/parser/exchange/anchor.rs
+++ espr-runtime/src/parser/exchange/anchor.rs
@@ -1,23 +1,40 @@
-use crate::parser::{combinator::*, exchange::*, token::*};
+use crate::parser::{combinator::*, token::*};
 use nom::{branch::alt, Parser};
 
 /// anchor_section = `ANCHOR;` [anchor_list] `ENDSEC;` .
-pub fn anchor_section(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn anchor_section(input: &str) -> ParseResult<Vec<Anchor>> {
+    tuple_((tag_("ANCHOR;"), anchor_list, tag_("ENDSEC;")))
+        .map(|(_start, anchors, _end)| anchors)
+        .parse(input)
 }
 
 /// anchor_list = { [anchor()] } .
-pub fn anchor_list(input: &str) -> ParseResult<()> {
-    todo!()
-}
-
-/// anchor = [anchor_name] `=` [anchor_item] { [anchor_tag] } `;` .
-pub fn anchor(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn anchor_list(input: &str) -> ParseResult<Vec<Anchor>> {
+    many0_(anchor).parse(input)
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Anchor {
+pub struct Anchor {
+    pub name: String,
+    pub item: AnchorItem,
+    pub tags: Vec<(String, AnchorItem)>,
+}
+
+/// anchor = [anchor_name] `=` [anchor_item] { [anchor_tag] } `;` .
+pub fn anchor(input: &str) -> ParseResult<Anchor> {
+    tuple_((
+        anchor_name,
+        char_('='),
+        anchor_item,
+        many0_(anchor_tag),
+        char_(';'),
+    ))
+    .map(|(name, _eq, item, tags, _semicolon)| Anchor { name, item, tags })
+    .parse(input)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum AnchorItem {
     Integer(i64),
     Real(f64),
     String(String),
@@ -27,18 +44,18 @@ pub enum Anchor {
     /// A reference to entity or value, parsed by [rhs_occurrence_name]
     RValue(RValue),
     /// List of other parameters
-    List(Vec<Anchor>),
+    List(Vec<AnchorItem>),
 }
 
 /// anchor_item = `$` | [integer] | [real] | [string] | [enumeration] | binary | [rhs_occurrence_name] | [resource] | [anchor_item_list] .
-pub fn anchor_item(input: &str) -> ParseResult<Anchor> {
+pub fn anchor_item(input: &str) -> ParseResult<AnchorItem> {
     alt((
-        char_('$').map(|_| Anchor::NotProvided),
-        integer.map(|val| Anchor::Integer(val)),
-        real.map(|val| Anchor::Real(val)),
-        string.map(|val| Anchor::String(val)),
-        rhs_occurrence_name.map(|val| Anchor::RValue(val)),
-        enumeration.map(|val| Anchor::Enumeration(val)),
+        char_('$').map(|_| AnchorItem::NotProvided),
+        integer.map(|val| AnchorItem::Integer(val)),
+        real.map(|val| AnchorItem::Real(val)),
+        string.map(|val| AnchorItem::String(val)),
+        rhs_occurrence_name.map(|val| AnchorItem::RValue(val)),
+        enumeration.map(|val| AnchorItem::Enumeration(val)),
         // FIXME binary
         anchor_item_list,
     ))
@@ -46,13 +63,15 @@ pub fn anchor_item(input: &str) -> ParseResult<Anchor> {
 }
 
 /// anchor_item_list = `(` \[ [anchor_item] { `,` [anchor_item] } \] `)` .
-pub fn anchor_item_list(input: &str) -> ParseResult<Anchor> {
+pub fn anchor_item_list(input: &str) -> ParseResult<AnchorItem> {
     tuple_((char_('('), opt_(comma_separated(anchor_item)), char_(')')))
-        .map(|(_open, anchors, _close)| Anchor::List(anchors.unwrap_or_default()))
+        .map(|(_open, anchors, _close)| AnchorItem::List(anchors.unwrap_or_default()))
         .parse(input)
 }
 
 /// anchor_tag = `{` [tag_name] `:` [anchor_item] `}` .
-pub fn anchor_tag(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn anchor_tag(input: &str) -> ParseResult<(String, AnchorItem)> {
+    tuple_((char_('{'), tag_name, char_(':'), anchor_item, char_('}')))
+        .map(|(_open, name, _colon, item, _close)| (name, item))
+        .parse(input)
 }
