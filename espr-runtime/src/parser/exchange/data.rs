bb4cfe58109037302436b4bdb1de035cc9537088
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-02-24 05:16:25 +0900

    EntityInstance

--- espr-runtime/src/parser/exchange/data.rs
+++ espr-runtime/src/parser/exchange/data.rs
@@ -1,4 +1,5 @@
 use crate::parser::{combinator::*, exchange::*, token::*};
+use nom::{branch::alt, Parser};
 
 /// data_section = `DATA` \[ `(` [parameter_list] `)` \] `;` [entity_instance_list] `ENDSEC;` .
 pub fn data_section(input: &str) -> ParseResult<()> {
@@ -6,36 +7,64 @@ pub fn data_section(input: &str) -> ParseResult<()> {
 }
 
 /// entity_instance_list = { [entity_instance] } .
-pub fn entity_instance_list(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn entity_instance_list(input: &str) -> ParseResult<Vec<EntityInstance>> {
+    many0_(entity_instance).parse(input)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum EntityInstance {
+    Simple { name: String, record: Record },
+    Complex { name: String, subsuper: Vec<Record> },
 }
 
 /// entity_instance = [simple_entity_instance] | [complex_entity_instance] .
-pub fn entity_instance(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn entity_instance(input: &str) -> ParseResult<EntityInstance> {
+    alt((simple_entity_instance, complex_entity_instance)).parse(input)
 }
 
 /// simple_entity_instance = [entity_instance_name] `=` [simple_record] `;` .
-pub fn simple_entity_instance(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn simple_entity_instance(input: &str) -> ParseResult<EntityInstance> {
+    tuple_((entity_instance_name, char_('='), simple_record, char_(';')))
+        .map(|(name, _eq, record, _semicolon)| EntityInstance::Simple { name, record })
+        .parse(input)
 }
 
 /// complex_entity_instance = [entity_instance_name] `=` [subsuper_record] `;` .
-pub fn complex_entity_instance(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn complex_entity_instance(input: &str) -> ParseResult<EntityInstance> {
+    tuple_((
+        entity_instance_name,
+        char_('='),
+        subsuper_record,
+        char_(';'),
+    ))
+    .map(|(name, _eq, subsuper, _semicolon)| EntityInstance::Complex { name, subsuper })
+    .parse(input)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Record {
+    pub name: String,
+    pub parameters: Vec<Parameter>,
 }
 
 /// simple_record = [keyword] `(` \[ [parameter_list] \] `)` .
-pub fn simple_record(input: &str) -> ParseResult<()> {
-    todo!()
-}
-
-/// subsuper_record = `(` [simple_record_list] `)` .
-pub fn subsuper_record(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn simple_record(input: &str) -> ParseResult<Record> {
+    tuple_((keyword, char_('('), opt_(parameter_list), char_(')')))
+        .map(|(name, _open, parameters, _close)| Record {
+            name,
+            parameters: parameters.unwrap_or_default(),
+        })
+        .parse(input)
 }
 
 /// simple_record_list = [simple_record] { [simple_record] } .
-pub fn simple_record_list(input: &str) -> ParseResult<()> {
-    todo!()
+pub fn simple_record_list(input: &str) -> ParseResult<Vec<Record>> {
+    many0_(simple_record).parse(input)
+}
+
+/// subsuper_record = `(` [simple_record_list] `)` .
+pub fn subsuper_record(input: &str) -> ParseResult<Vec<Record>> {
+    tuple_((char_('('), simple_record_list, char_(')')))
+        .map(|(_open, records, _close)| records)
+        .parse(input)
 }
--- espr-runtime/src/parser/exchange/header.rs
+++ espr-runtime/src/parser/exchange/header.rs
@@ -1,30 +1,21 @@
-use crate::parser::{combinator::*, exchange::*, token::*};
+use crate::parser::{combinator::*, exchange::*};
 use nom::Parser;
 
 /// header_section = `HEADER;` [header_entity] [header_entity] [header_entity] \[ [header_entity_list] \] `ENDSEC;` .
-pub fn header_section(input: &str) -> ParseResult<Vec<HeaderEntity>> {
+pub fn header_section(input: &str) -> ParseResult<Vec<Record>> {
     tuple_((tag_("HEADER;"), header_entity_list, tag_("ENDSEC;")))
         .map(|(_start, entities, _close)| entities)
         .parse(input)
 }
 
 /// header_entity_list = [header_entity] { [header_entity] } .
-pub fn header_entity_list(input: &str) -> ParseResult<Vec<HeaderEntity>> {
+pub fn header_entity_list(input: &str) -> ParseResult<Vec<Record>> {
     many1_(header_entity).parse(input)
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct HeaderEntity {
-    pub name: String,
-    pub parameters: Vec<Parameter>,
-}
-
 /// header_entity = [keyword] `(` \[ [parameter_list] \] `)` `;` .
-pub fn header_entity(input: &str) -> ParseResult<HeaderEntity> {
-    tuple_((keyword, char_('('), opt_(parameter_list), char_(')')))
-        .map(|(name, _open, parameters, _close)| HeaderEntity {
-            name,
-            parameters: parameters.unwrap_or_default(),
-        })
+pub fn header_entity(input: &str) -> ParseResult<Record> {
+    tuple_((simple_record, char_(';')))
+        .map(|(record, _semicolon)| record)
         .parse(input)
 }
