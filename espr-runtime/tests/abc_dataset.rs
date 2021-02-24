058709326223b7087dcc8b3f13527b63ec9bcb68
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-02-25 04:37:20 +0900

    Test parse entire abc_dataset

--- espr-runtime/tests/abc_dataset.rs
+++ espr-runtime/tests/abc_dataset.rs
@@ -1,6 +1,15 @@
-/// Integration test for parsing STEP file downloaded from ABC Dataset
+//! Integration test for parsing STEP file downloaded from ABC Dataset
+
+use espr_runtime::parser;
+use nom::Finish;
+use std::{fs, path::*};
 
 #[test]
-fn abc_dataset() {
-    todo!()
+fn abc_dataset() -> anyhow::Result<()> {
+    let step_file = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("tests/steps/00000050_80d90bfdd2e74e709956122a_step_000.step");
+    let step_str = fs::read_to_string(step_file)?;
+    let exchange = parser::exchange::exchange_file(&step_str).finish().unwrap();
+    dbg!(exchange);
+    Ok(())
 }
