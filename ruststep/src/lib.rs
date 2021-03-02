08246e350fbcf54e1b13607716b22251a061e86f
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-03-02 18:44:08 +0900

    Merge ruststep::{logial,array} as ruststep::primitive

--- ruststep/src/lib.rs
+++ ruststep/src/lib.rs
@@ -1,6 +1,13 @@
-pub mod array;
-pub mod logical;
-pub mod parser;
+//! Reference of ruststep, a STEP toolkit written in pure Rust
+//!
+//! See [README.md][README] and [ARCHITECTURE.md][ARCHITECTURE] on [GitHub][GitHub]
+//! for the overview and high-level architecture of this project.
+//! This reference focus on the detail usage of this crate.
+//!
+//! [GitHub]: https://github.com/ricosjp/ruststep
+//! [README]: https://github.com/ricosjp/ruststep/blob/master/README.md
+//! [ARCHITECTURE]: https://github.com/ricosjp/ruststep/blob/master/ARCHITECTURE.md
+//!
 
-pub use array::Array;
-pub use logical::Logical;
+pub mod parser;
+pub mod primitive;
---
+++ ruststep/src/primitive/mod.rs
@@ -0,0 +1,7 @@
+//! Primitive types exists in STEP but not in Rust
+
+mod array;
+mod logical;
+
+pub use array::*;
+pub use logical::*;
