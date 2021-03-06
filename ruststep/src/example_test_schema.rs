b4f0daac9499b5b54b0a53dd218c3f2f3f547fd6
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-03-06 09:59:26 +0900

    Rename B into BEntry, and create B { u64, A }

--- ruststep/src/example_test_schema.rs
+++ ruststep/src/example_test_schema.rs
@@ -21,7 +21,7 @@ type Table<T> = HashMap<usize, T>;
 #[derive(Debug)]
 pub struct Ap000 {
     a: Table<A>,
-    b: Table<B>,
+    b: Table<BEntry>,
 }
 
 impl Ap000 {
@@ -30,7 +30,7 @@ impl Ap000 {
     }
 
     pub fn b_iter(&self) -> impl Iterator<Item = (&usize, BRef)> {
-        self.b.iter().map(move |(id, B { z, w })| {
+        self.b.iter().map(move |(id, BEntry { z, w })| {
             (
                 id,
                 BRef {
@@ -42,7 +42,7 @@ impl Ap000 {
     }
 
     pub fn b_get(&self, id: usize) -> Option<BRef> {
-        let B { z, w } = &self.b.get(&id)?;
+        let BEntry { z, w } = &self.b.get(&id)?;
         Some(BRef {
             z,
             w: self.a.get(w).unwrap(),
@@ -50,7 +50,7 @@ impl Ap000 {
     }
 
     pub fn b_get_mut(&mut self, id: usize) -> Option<BMut> {
-        let B { z, w } = self.b.get_mut(&id)?;
+        let BEntry { z, w } = self.b.get_mut(&id)?;
         Some(BMut {
             z,
             w: self.a.get_mut(w).unwrap(),
@@ -58,26 +58,73 @@ impl Ap000 {
     }
 }
 
-#[derive(Debug, PartialEq, Hash)]
+#[derive(Debug, Clone, PartialEq, Hash)]
 pub struct A {
-    x: u64,
-    y: u64,
+    pub x: u64,
+    pub y: u64,
+}
+
+#[derive(Debug, Clone, PartialEq, Hash)]
+pub struct B {
+    pub z: u64,
+    pub w: A,
 }
 
 #[derive(Debug, PartialEq, Hash)]
-struct B {
+struct BEntry {
     z: u64,
     w: usize, // ref to A
 }
 
-#[derive(Debug, PartialEq, Hash)]
+/// Element-wise immutable reference to [B]
+#[derive(Debug, Clone, PartialEq, Hash)]
 pub struct BRef<'schema> {
-    z: &'schema u64,
-    w: &'schema A,
+    pub z: &'schema u64,
+    pub w: &'schema A,
 }
 
+impl<'schema> From<&'schema B> for BRef<'schema> {
+    fn from(b: &'schema B) -> Self {
+        let B { z, w } = b;
+        BRef { z, w }
+    }
+}
+
+impl<'schema> From<BMut<'schema>> for BRef<'schema> {
+    fn from(b: BMut<'schema>) -> Self {
+        let BMut { z, w } = b;
+        BRef { z, w }
+    }
+}
+
+impl<'schema> BRef<'schema> {
+    pub fn to_owned(&self) -> B {
+        B {
+            z: self.z.clone(),
+            w: self.w.clone(),
+        }
+    }
+}
+
+/// Element-wise mutable reference to [B]
 #[derive(Debug, PartialEq, Hash)]
 pub struct BMut<'schema> {
-    z: &'schema mut u64,
-    w: &'schema mut A,
+    pub z: &'schema mut u64,
+    pub w: &'schema mut A,
+}
+
+impl<'schema> From<&'schema mut B> for BMut<'schema> {
+    fn from(b: &'schema mut B) -> Self {
+        let B { z, w } = b;
+        BMut { z, w }
+    }
+}
+
+impl<'schema> BMut<'schema> {
+    pub fn to_owned(&self) -> B {
+        B {
+            z: self.z.clone(),
+            w: self.w.clone(),
+        }
+    }
 }
