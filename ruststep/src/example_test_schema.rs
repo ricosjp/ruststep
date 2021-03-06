823ff8c5c5a7193f4c2633b8b50f3b378246175c
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-03-06 14:06:33 +0900

    Drop BMut
    Because *Mut reference cannot assure the single mutable reference
    in this graph structure.

--- ruststep/src/example_test_schema.rs
+++ ruststep/src/example_test_schema.rs
@@ -25,35 +25,14 @@ pub struct Ap000 {
 }
 
 impl Ap000 {
-    pub fn a_iter(&self) -> impl Iterator<Item = (&usize, &A)> {
-        self.a.iter()
+    pub fn a_iter(&self) -> impl Iterator<Item = &A> {
+        self.a.iter().map(|(_id, a)| a)
     }
 
-    pub fn b_iter(&self) -> impl Iterator<Item = (&usize, BRef)> {
-        self.b.iter().map(move |(id, BEntry { z, w })| {
-            (
-                id,
-                BRef {
-                    z,
-                    w: self.a.get(w).unwrap(),
-                },
-            )
-        })
-    }
-
-    pub fn b_get(&self, id: usize) -> Option<BRef> {
-        let BEntry { z, w } = &self.b.get(&id)?;
-        Some(BRef {
+    pub fn b_iter(&self) -> impl Iterator<Item = BRef> {
+        self.b.iter().map(move |(_id, BEntry { z, w })| BRef {
             z,
             w: self.a.get(w).unwrap(),
-        })
-    }
-
-    pub fn b_get_mut(&mut self, id: usize) -> Option<BMut> {
-        let BEntry { z, w } = self.b.get_mut(&id)?;
-        Some(BMut {
-            z,
-            w: self.a.get_mut(w).unwrap(),
         })
     }
 }
@@ -90,37 +69,7 @@ impl<'schema> From<&'schema B> for BRef<'schema> {
     }
 }
 
-impl<'schema> From<BMut<'schema>> for BRef<'schema> {
-    fn from(b: BMut<'schema>) -> Self {
-        let BMut { z, w } = b;
-        BRef { z, w }
-    }
-}
-
 impl<'schema> BRef<'schema> {
-    pub fn to_owned(&self) -> B {
-        B {
-            z: self.z.clone(),
-            w: self.w.clone(),
-        }
-    }
-}
-
-/// Element-wise mutable reference to [B]
-#[derive(Debug, PartialEq, Hash)]
-pub struct BMut<'schema> {
-    pub z: &'schema mut u64,
-    pub w: &'schema mut A,
-}
-
-impl<'schema> From<&'schema mut B> for BMut<'schema> {
-    fn from(b: &'schema mut B) -> Self {
-        let B { z, w } = b;
-        BMut { z, w }
-    }
-}
-
-impl<'schema> BMut<'schema> {
     pub fn to_owned(&self) -> B {
         B {
             z: self.z.clone(),
