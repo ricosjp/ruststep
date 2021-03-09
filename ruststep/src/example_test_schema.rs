15fec87a9639702967005a4c6ac654fc2fea0308
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-03-09 17:57:12 +0900

    impl Debug, Hash for Id<T>

--- ruststep/src/example_test_schema.rs
+++ ruststep/src/example_test_schema.rs
@@ -19,14 +19,23 @@
 //! END_SCHEMA;
 //! ```
 
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fmt,
+    hash::{Hash, Hasher},
+};
 
 type Table<T> = HashMap<Id<T>, T>;
 
-#[derive(Debug, Hash)]
 pub struct Id<T: 'static> {
     id: usize,
     phantom: std::marker::PhantomData<&'static T>,
+}
+
+impl<T: 'static> fmt::Debug for Id<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.id)
+    }
 }
 
 impl<T: 'static> PartialEq for Id<T> {
@@ -36,6 +45,12 @@ impl<T: 'static> PartialEq for Id<T> {
 }
 
 impl<T: 'static> Eq for Id<T> {}
+
+impl<T: 'static> Hash for Id<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
 
 pub trait Entity<'rf> {
     type Schema: EntryTable<'rf, Self::Entry>;
