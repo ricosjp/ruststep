914292c1ea2f89e2d73a5b6561f2437ad9cd7d45
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-03-18 17:33:02 +0900

    Drop unused errors due to serde

--- ruststep/src/error.rs
+++ ruststep/src/error.rs
@@ -4,19 +4,6 @@ use std::fmt;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, Clone, Hash)]
-pub enum TypeKind {
-    // Primitive types in the exchange structure spec
-    Integer,
-    Real,
-    String,
-    Enumeration,
-    List,
-
-    // Defined in a schema
-    Entity(String),
-}
-
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error(transparent)]
@@ -24,23 +11,6 @@ pub enum Error {
 
     #[error(transparent)]
     InvalidHeader(#[from] InvalidHeader),
-
-    #[error(
-        "Number of parameters mismatch: entity={entity}, expected={expected}, actual={actual}"
-    )]
-    ParameterSizeMismatch {
-        entity: String,
-        expected: u32,
-        actual: u32,
-    },
-
-    #[error("Type mismatch at {entity}:{position}, expected={expected:?}, actual={actual:?}")]
-    TypeMismatch {
-        entity: String,
-        position: u32,
-        expected: TypeKind,
-        actual: TypeKind,
-    },
 
     #[error("Error while deserialize STEP struct: {0}")]
     DeserializeFailed(String),
