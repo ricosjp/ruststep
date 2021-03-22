ad3993a4fba0f5134d366a0546199251636cdb8d
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-03-23 01:17:40 +0900

    Merge UntypedParameter into Parameter

--- ruststep/src/parser/exchange/parameter.rs
+++ ruststep/src/parser/exchange/parameter.rs
@@ -2,27 +2,6 @@ use crate::parser::{combinator::*, token::*};
 use nom::{branch::alt, combinator::value, Parser};
 use serde::{de, forward_to_deserialize_any};
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum UntypedParameter {
-    Integer(i64),
-    Real(f64),
-    String(String),
-    Enumeration(String),
-    /// The special token dollar sign (`$`) is used to represent an object whose value is not provided in the exchange structure.
-    NotProvided,
-    /// A reference to entity or value, parsed by [rhs_occurrence_name]
-    RValue(RValue),
-    /// List of other parameters
-    List(Vec<Parameter>),
-}
-
-/// list = `(` \[ [parameter] { `,` [parameter] } \] `)` .
-pub fn list(input: &str) -> ParseResult<UntypedParameter> {
-    tuple_((char_('('), comma_separated(parameter), char_(')')))
-        .map(|(_open, params, _close)| UntypedParameter::List(params))
-        .parse(input)
-}
-
 /// Primitive value type in STEP data, parsed by [parameter]
 ///
 /// Parse
@@ -30,7 +9,7 @@ pub fn list(input: &str) -> ParseResult<UntypedParameter> {
 ///
 /// ```
 /// use nom::Finish;
-/// use ruststep::parser::{Parameter, UntypedParameter, exchange};
+/// use ruststep::parser::{Parameter, exchange};
 ///
 /// // Real number
 /// let (residual, p) = exchange::parameter("1.0").finish().unwrap();
@@ -55,16 +34,15 @@ pub fn list(input: &str) -> ParseResult<UntypedParameter> {
 ///
 /// FromIterator
 /// -------------
-/// Create a list as `Parameter::Untyped(UntypedParameter::List)` from `Iterator<Item=Parameter>`
-/// or `Iterator<Item=&Parameter>`.
+/// Create a list as `Parameter::List` from `Iterator<Item=Parameter>` or `Iterator<Item=&Parameter>`.
 ///
 /// ```
-/// use ruststep::parser::{Parameter, UntypedParameter};
+/// use ruststep::parser::Parameter;
 ///
 /// let p: Parameter = [Parameter::real(1.0), Parameter::real(2.0)]
 ///     .iter()
 ///     .collect();
-/// assert!(matches!(p, Parameter::Untyped(UntypedParameter::List(_))));
+/// assert!(matches!(p, Parameter::List(_)));
 /// ```
 ///
 /// serde::Deserializer
@@ -82,7 +60,7 @@ pub fn list(input: &str) -> ParseResult<UntypedParameter> {
 ///     y: f64,
 /// }
 ///
-/// // Create a list as `Parameter::Untyped(UntypedParameter::List)`
+/// // Create a list as `Parameter::Untyped(Parameter::List)`
 /// let p: Parameter = [Parameter::real(1.0), Parameter::real(2.0)]
 ///     .iter()
 ///     .collect();
@@ -103,24 +81,37 @@ pub fn list(input: &str) -> ParseResult<UntypedParameter> {
 #[derive(Debug, Clone, PartialEq)]
 pub enum Parameter {
     /// Inline *Typed* struct
-    Typed { name: String, ty: Box<Parameter> },
-    /// Primitive types e.g. integer. See [UntypedParameter] for detail.
-    Untyped(UntypedParameter),
+    Typed {
+        name: String,
+        ty: Box<Parameter>,
+    },
+
+    Integer(i64),
+    Real(f64),
+    String(String),
+    Enumeration(String),
+    /// A reference to entity or value, parsed by [rhs_occurrence_name]
+    RValue(RValue),
+    /// List of other parameters
+    List(Vec<Parameter>),
+
+    /// The special token dollar sign (`$`) is used to represent an object whose value is not provided in the exchange structure.
+    NotProvided,
     /// Omitted parameter denoted by `*`
     Omitted,
 }
 
 impl Parameter {
     pub fn integer(i: i64) -> Self {
-        Parameter::Untyped(UntypedParameter::Integer(i))
+        Parameter::Integer(i)
     }
 
     pub fn real(x: f64) -> Self {
-        Parameter::Untyped(UntypedParameter::Real(x))
+        Parameter::Real(x)
     }
 
     pub fn string(s: &str) -> Self {
-        Parameter::Untyped(UntypedParameter::String(s.to_string()))
+        Parameter::String(s.to_string())
     }
 }
 
@@ -138,13 +129,13 @@ impl From<f64> for Parameter {
 
 impl From<String> for Parameter {
     fn from(value: String) -> Self {
-        Parameter::Untyped(UntypedParameter::String(value))
+        Parameter::String(value)
     }
 }
 
 impl std::iter::FromIterator<Parameter> for Parameter {
     fn from_iter<Iter: IntoIterator<Item = Parameter>>(iter: Iter) -> Self {
-        Parameter::Untyped(UntypedParameter::List(iter.into_iter().collect()))
+        Parameter::List(iter.into_iter().collect())
     }
 }
 
@@ -152,6 +143,13 @@ impl<'a> std::iter::FromIterator<&'a Parameter> for Parameter {
     fn from_iter<Iter: IntoIterator<Item = &'a Parameter>>(iter: Iter) -> Self {
         iter.into_iter().cloned().collect()
     }
+}
+
+/// list = `(` \[ [parameter] { `,` [parameter] } \] `)` .
+pub fn list(input: &str) -> ParseResult<Parameter> {
+    tuple_((char_('('), comma_separated(parameter), char_(')')))
+        .map(|(_open, params, _close)| Parameter::List(params))
+        .parse(input)
 }
 
 /// parameter = [typed_parameter] | [untyped_parameter] | [omitted_parameter] .
@@ -172,16 +170,15 @@ pub fn typed_parameter(input: &str) -> ParseResult<Parameter> {
 /// untyped_parameter = `$` | [integer] | [real] | [string] | [rhs_occurrence_name] | [enumeration] | binary | [list] .
 pub fn untyped_parameter(input: &str) -> ParseResult<Parameter> {
     alt((
-        char_('$').map(|_| UntypedParameter::NotProvided),
-        real.map(UntypedParameter::Real),
-        integer.map(UntypedParameter::Integer),
-        string.map(UntypedParameter::String),
-        rhs_occurrence_name.map(UntypedParameter::RValue),
-        enumeration.map(UntypedParameter::Enumeration),
+        char_('$').map(|_| Parameter::NotProvided),
+        real.map(Parameter::Real),
+        integer.map(Parameter::Integer),
+        string.map(Parameter::String),
+        rhs_occurrence_name.map(Parameter::RValue),
+        enumeration.map(Parameter::Enumeration),
         // FIXME binary
         list,
     ))
-    .map(Parameter::Untyped)
     .parse(input)
 }
 
@@ -204,21 +201,19 @@ impl<'de, 'param> de::Deserializer<'de> for &'param Parameter {
     {
         match self {
             Parameter::Typed { name: _, ty: _ } => unimplemented!(),
-            Parameter::Untyped(p) => match p {
-                UntypedParameter::Integer(val) => visitor.visit_i64(*val),
-                UntypedParameter::Real(val) => visitor.visit_f64(*val),
-                UntypedParameter::String(val) => visitor.visit_str(val),
-                UntypedParameter::List(params) => {
-                    let seq = de::value::SeqDeserializer::new(params.iter());
-                    visitor.visit_seq(seq)
-                }
-                UntypedParameter::RValue(rvalue) => match rvalue {
-                    RValue::Value(id) | RValue::Entity(id) => visitor.visit_u64(*id),
-                    RValue::ConstantValue(c) | RValue::ConstantEntity(c) => visitor.visit_str(c),
-                },
-                _ => unimplemented!(),
+            Parameter::Integer(val) => visitor.visit_i64(*val),
+            Parameter::Real(val) => visitor.visit_f64(*val),
+            Parameter::String(val) => visitor.visit_str(val),
+            Parameter::List(params) => {
+                let seq = de::value::SeqDeserializer::new(params.iter());
+                visitor.visit_seq(seq)
+            }
+            Parameter::RValue(rvalue) => match rvalue {
+                RValue::Value(id) | RValue::Entity(id) => visitor.visit_u64(*id),
+                RValue::ConstantValue(c) | RValue::ConstantEntity(c) => visitor.visit_str(c),
             },
             Parameter::Omitted => unimplemented!(),
+            _ => unimplemented!(),
         }
     }
 
@@ -246,19 +241,19 @@ mod tests {
         let l: Parameter = [Parameter::integer(1), Parameter::real(2.0)]
             .iter()
             .collect();
-        assert!(matches!(l, Parameter::Untyped(UntypedParameter::List(_))));
+        assert!(matches!(l, Parameter::List(_)));
     }
 
     #[test]
     fn deserialize_int() {
-        let p = Parameter::Untyped(UntypedParameter::Integer(2));
+        let p = Parameter::Integer(2);
         let a: i64 = Deserialize::deserialize(&p).unwrap();
         assert_eq!(a, 2);
         // can be deserialized as unsigned
         let a: u32 = Deserialize::deserialize(&p).unwrap();
         assert_eq!(a, 2);
 
-        let p = Parameter::Untyped(UntypedParameter::Integer(-2));
+        let p = Parameter::Integer(-2);
         let a: i64 = Deserialize::deserialize(&p).unwrap();
         assert_eq!(a, -2);
         // cannot be deserialized negative integer into unsigned
