b9c9471841ceaa10ef721fe34c4c5066e8d68dbf
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-03-28 23:22:25 +0900

    Identity deserialize/deserializer for Parameter

--- ruststep/src/parser/exchange/parameter.rs
+++ ruststep/src/parser/exchange/parameter.rs
@@ -2,6 +2,7 @@ use crate::parser::{combinator::*, token::*, value::*};
 use inflector::Inflector;
 use nom::{branch::alt, combinator::value, Parser};
 use serde::{de, forward_to_deserialize_any, Deserialize};
+use std::fmt;
 
 /// Primitive value type in STEP data, parsed by [parameter]
 ///
@@ -151,7 +152,7 @@ use serde::{de, forward_to_deserialize_any, Deserialize};
 ///
 /// [serde::Deserializer]: https://docs.serde.rs/serde/trait.Deserializer.html
 ///
-#[derive(Debug, Clone, PartialEq, Deserialize)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Parameter {
     /// Inline *Typed* struct
     Typed { name: String, ty: Box<Parameter> },
@@ -275,7 +276,9 @@ impl<'de, 'param> de::Deserializer<'de> for &'param Parameter {
         V: de::Visitor<'de>,
     {
         match self {
-            Parameter::Typed { .. } => unimplemented!(),
+            Parameter::Typed { name, ty } => visitor.visit_map(de::value::MapDeserializer::new(
+                [(name.as_str(), &**ty)].iter().cloned(),
+            )),
             Parameter::Integer(val) => visitor.visit_i64(*val),
             Parameter::Real(val) => visitor.visit_f64(*val),
             Parameter::String(val) => visitor.visit_str(val),
@@ -317,10 +320,70 @@ impl<'de, 'param> de::Deserializer<'de> for &'param Parameter {
     }
 }
 
+// To support SeqDeserializer
 impl<'de, 'param> de::IntoDeserializer<'de, crate::error::Error> for &'param Parameter {
     type Deserializer = Self;
     fn into_deserializer(self) -> Self::Deserializer {
         self
+    }
+}
+
+impl<'de> Deserialize<'de> for Parameter {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        deserializer.deserialize_any(ParameterVisitor {})
+    }
+}
+
+struct ParameterVisitor {}
+
+impl<'de> de::Visitor<'de> for ParameterVisitor {
+    type Value = Parameter;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("Parameter")
+    }
+
+    fn visit_i64<E>(self, value: i64) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        Ok(Parameter::integer(value))
+    }
+
+    fn visit_f64<E>(self, value: f64) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        Ok(Parameter::real(value))
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        Ok(Parameter::string(value))
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: de::SeqAccess<'de>,
+    {
+        let mut components: Vec<Parameter> = Vec::new();
+        while let Some(component) = seq.next_element()? {
+            components.push(component);
+        }
+        Ok(Parameter::List(components))
+    }
+
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: de::MapAccess<'de>,
+    {
+        let (name, ty) = map.next_entry()?.unwrap();
+        Ok(Parameter::Typed { name, ty })
     }
 }
 
@@ -345,6 +408,34 @@ mod tests {
         // cannot be deserialized negative integer into unsigned
         let res: Result<u32, _> = Deserialize::deserialize(&p);
         assert!(res.is_err());
+    }
+
+    #[test]
+    fn deserialize_identity() {
+        let p = Parameter::integer(2);
+        let q: Parameter = Deserialize::deserialize(&p).unwrap();
+        assert_eq!(p, q);
+
+        let p = Parameter::real(2.0);
+        let q: Parameter = Deserialize::deserialize(&p).unwrap();
+        assert_eq!(p, q);
+
+        let p = Parameter::string("vim");
+        let q: Parameter = Deserialize::deserialize(&p).unwrap();
+        assert_eq!(p, q);
+
+        let p: Parameter = [Parameter::integer(1), Parameter::real(2.0)]
+            .iter()
+            .collect();
+        let q: Parameter = Deserialize::deserialize(&p).unwrap();
+        assert_eq!(p, q);
+
+        let (res, p) = super::parameter("B((1.0, A((2.0, 3.0))))")
+            .finish()
+            .unwrap();
+        assert_eq!(res, "");
+        let q: Parameter = Deserialize::deserialize(&p).unwrap();
+        assert_eq!(p, q);
     }
 
     #[derive(Debug, Deserialize)]
