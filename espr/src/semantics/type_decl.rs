bc26f43839f06bbd0dd505b1ead0dc417f6f776f
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-05-04 16:38:56 +0900

    ToTokens for Rename, Enumeration, and Select

--- espr/src/semantics/type_decl.rs
+++ espr/src/semantics/type_decl.rs
@@ -36,6 +36,16 @@ pub struct Rename {
     ty: TypeRef,
 }
 
+impl ToTokens for Rename {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let id = format_ident!("{}", &self.id.to_pascal_case());
+        let ty = &self.ty;
+        tokens.append_all(quote! {
+            pub type #id = #ty;
+        });
+    }
+}
+
 /// Enumeration of values,
 /// e.g. `TYPE text_path = ENUMERATION OF (up, right, down, left); END_TYPE;`
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -44,12 +54,65 @@ pub struct Enumeration {
     values: Vec<String>,
 }
 
+impl ToTokens for Enumeration {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let id = format_ident!("{}", &self.id.to_pascal_case());
+        let items: Vec<_> = self
+            .values
+            .iter()
+            .map(|i| format_ident!("{}", i.to_pascal_case()))
+            .collect();
+        tokens.append_all(quote! {
+            #[derive(Debug, Clone)]
+            pub enum #id {
+                #( #items ),*
+            }
+        });
+    }
+}
+
 /// Select of user defined types,
 /// e.g. `TYPE geometric_set_select = SELECT (point, curve); END_TYPE;`
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Select {
     id: String,
     types: Vec<TypeRef>,
+}
+
+impl ToTokens for Select {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let id = format_ident!("{}", &self.id.to_pascal_case());
+        let mut entries = Vec::new();
+        let mut entry_types = Vec::new();
+        for ty in &self.types {
+            match ty {
+                TypeRef::Entity {
+                    name,
+                    has_supertype_decl,
+                    ..
+                } => {
+                    let name = format_ident!("{}", name.to_pascal_case());
+                    entries.push(quote! { #name });
+                    if *has_supertype_decl {
+                        // avoid Box<Box<XxxAny>>
+                        entry_types.push(quote! { #ty });
+                    } else {
+                        entry_types.push(quote! { Box<#ty> });
+                    }
+                }
+                _ => {
+                    entries.push(ty.to_token_stream());
+                    entry_types.push(quote! { Box<#ty> });
+                }
+            }
+        }
+        tokens.append_all(quote! {
+            #[derive(Debug, Clone)]
+            pub enum #id {
+                #(#entries(#entry_types)),*
+            }
+        });
+    }
 }
 
 impl Legalize for UnderlyingType {
