43828c76a092a0157714e96c8df932d74eadb30a
Greg Rauhoeft <greg.rauhoeft@ipa.fraunhofer.de>
2021-05-21 14:55:44 +0200

    Changed the order of operations in attribute_decl, because by allowing upper- and lowercase characters in IDs makes attribute_id recognize things that are actually redeclared_attributes.

--- espr/src/parser/entity/attribute.rs
+++ espr/src/parser/entity/attribute.rs
@@ -6,8 +6,8 @@ use crate::{
 /// 177 attribute_decl = [attribute_id] | [redeclared_attribute] .
 pub fn attribute_decl(input: &str) -> ParseResult<AttributeDecl> {
     alt((
-        attribute_id.map(AttributeDecl::Reference),
         redeclared_attribute,
+        attribute_id.map(AttributeDecl::Reference),
     ))
     .parse(input)
 }
