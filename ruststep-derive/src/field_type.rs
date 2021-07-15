8b9e0a12cace3be3a1b59c927db3e0854df9ff9c
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-07-16 02:49:34 +0900

    impl Into<syn::Type> and TryFrom<syn::Type> for FieldType

--- ruststep-derive/src/field_type.rs
+++ ruststep-derive/src/field_type.rs
@@ -1,3 +1,5 @@
+use std::convert::{TryFrom, TryInto};
+
 /// espr-generated field type
 #[derive(Debug, Clone)]
 pub enum FieldType {
@@ -9,38 +11,60 @@ pub enum FieldType {
     List(Box<FieldType>),
 }
 
-impl FieldType {
-    pub fn new(ty: &syn::Type) -> Self {
+#[derive(Debug, Clone)]
+pub struct UnsupportedTypeError {}
+
+impl Into<syn::Type> for FieldType {
+    fn into(self) -> syn::Type {
+        let path = match self {
+            FieldType::Path(path) => path,
+            FieldType::Optional(ty) => {
+                let ty: syn::Type = (*ty).into();
+                syn::parse_quote! { Option<#ty> }
+            }
+            FieldType::List(ty) => {
+                let ty: syn::Type = (*ty).into();
+                syn::parse_quote! { Vec<#ty> }
+            }
+        };
+        syn::Type::Path(syn::TypePath { qself: None, path })
+    }
+}
+
+impl TryFrom<syn::Type> for FieldType {
+    type Error = UnsupportedTypeError;
+
+    fn try_from(ty: syn::Type) -> Result<Self, Self::Error> {
         let path = if let syn::Type::Path(syn::TypePath { path, .. }) = ty {
             path
         } else {
-            panic!("non-path field is not supported")
+            return Err(UnsupportedTypeError {});
         };
 
         let syn::Path { segments, .. } = &path;
         let last_seg = segments.last().unwrap();
 
         match &last_seg.arguments {
-            syn::PathArguments::None => FieldType::Path(path.clone()),
+            syn::PathArguments::None => Ok(FieldType::Path(path.clone())),
             syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
                 args,
                 ..
             }) => {
                 assert_eq!(args.len(), 1);
                 if let syn::GenericArgument::Type(ty) = &args[0] {
-                    let ty = Box::new(FieldType::new(&ty));
+                    let ty = Box::new(ty.clone().try_into()?);
                     if last_seg.ident == "Option" {
-                        return FieldType::Optional(ty);
+                        return Ok(FieldType::Optional(ty));
                     }
                     if last_seg.ident == "Vec" {
-                        return FieldType::List(ty);
+                        return Ok(FieldType::List(ty));
                     }
-                    unreachable!("espr emits unexpected code")
+                    return Err(UnsupportedTypeError {});
                 } else {
-                    unreachable!("espr emits unexpected code")
+                    return Err(UnsupportedTypeError {});
                 }
             }
-            _ => unreachable!("espr emits unexpected code"),
+            _ => Err(UnsupportedTypeError {}),
         }
     }
 }
@@ -50,25 +74,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn field_type_new() {
+    fn field_type_convert() {
         let ty: syn::Type = syn::parse_str("T").unwrap();
-        let f = FieldType::new(&ty);
+        let f = ty.clone().try_into().unwrap();
         assert!(matches!(f, FieldType::Path(_)));
+        assert_eq!(<FieldType as Into<syn::Type>>::into(f), ty);
 
         let ty: syn::Type = syn::parse_str("Option<T>").unwrap();
-        let f = FieldType::new(&ty);
+        let f = ty.clone().try_into().unwrap();
         assert!(matches!(f, FieldType::Optional(_)));
+        assert_eq!(<FieldType as Into<syn::Type>>::into(f), ty);
 
         let ty: syn::Type = syn::parse_str("Vec<T>").unwrap();
-        let f = FieldType::new(&ty);
+        let f = ty.clone().try_into().unwrap();
         assert!(matches!(f, FieldType::List(_)));
+        assert_eq!(<FieldType as Into<syn::Type>>::into(f), ty);
 
         let ty: syn::Type = syn::parse_str("Option<Vec<T>>").unwrap();
-        let f = FieldType::new(&ty);
-        if let FieldType::Optional(ty) = f {
-            assert!(matches!(*ty, FieldType::List(_)));
+        let f = ty.clone().try_into().unwrap();
+        if let FieldType::Optional(ty) = &f {
+            assert!(matches!(**ty, FieldType::List(_)));
         } else {
             panic!()
         }
+        assert_eq!(<FieldType as Into<syn::Type>>::into(f), ty);
     }
 }
