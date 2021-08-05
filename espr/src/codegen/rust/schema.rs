c81b7dc85a94a99de1290d24092141776740377a
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-08-06 08:55:32 +0900

    Use ruststep_derive::as_holder!

--- espr/src/codegen/rust/schema.rs
+++ espr/src/codegen/rust/schema.rs
@@ -9,17 +9,13 @@ impl ToTokens for Schema {
         let name = format_ident!("{}", self.name);
         let types = &self.types;
         let entities = &self.entities;
-        let entity_name: Vec<_> = entities
+        let entity_types: Vec<_> = entities
             .iter()
             .map(|e| format_ident!("{}", e.name.to_pascal_case()))
             .collect();
         let holder_name: Vec<_> = entities
             .iter()
             .map(|e| format_ident!("{}", e.name))
-            .collect();
-        let holder_type: Vec<_> = entities
-            .iter()
-            .map(|e| format_ident!("{}Holder", e.name.to_pascal_case()))
             .collect();
         let iter_name: Vec<_> = entities
             .iter()
@@ -29,18 +25,19 @@ impl ToTokens for Schema {
             pub mod #name {
                 use crate::{primitive::*, tables::*, error::Result};
                 use std::collections::HashMap;
+                use ruststep_derive::as_holder;
 
                 #[derive(Debug, Clone, PartialEq, Default)]
                 pub struct Tables {
                     #(
-                    #holder_name: HashMap<u64, #holder_type>,
+                    #holder_name: HashMap<u64, as_holder!(#entity_types)>,
                     )*
                 }
 
                 impl Tables {
                     #(
                     pub fn #iter_name<'table>(&'table self) ->
-                        impl Iterator<Item = Result<#entity_name>> + 'table
+                        impl Iterator<Item = Result<#entity_types>> + 'table
                     {
                         self.#holder_name
                             .values()
