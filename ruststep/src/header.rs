e361d6c64360659ea2de468279873d995b987acd
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-08-16 21:10:29 +0900

    Manually impl Deserialize for header struts

--- ruststep/src/header.rs
+++ ruststep/src/header.rs
@@ -8,11 +8,52 @@ pub struct FileDescription {
 }
 
 impl<'de> de::Deserialize<'de> for FileDescription {
-    fn deserialize<D>(_deserializer: D) -> ::std::result::Result<Self, D::Error>
+    fn deserialize<D>(deserializer: D) -> ::std::result::Result<Self, D::Error>
     where
         D: de::Deserializer<'de>,
     {
-        todo!()
+        deserializer.deserialize_tuple_struct("FILE_DESCRIPTION", 2, FileDescriptionVisitor {})
+    }
+}
+
+struct FileDescriptionVisitor;
+
+impl<'de> ::serde::de::Visitor<'de> for FileDescriptionVisitor {
+    type Value = FileDescription;
+    fn expecting(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(formatter, "FILE_DESCRIPTION")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> ::std::result::Result<Self::Value, A::Error>
+    where
+        A: ::serde::de::SeqAccess<'de>,
+    {
+        if let Some(size) = seq.size_hint() {
+            if size != 2 {
+                todo!("Create another error and send it")
+            }
+        }
+        let description = seq.next_element()?.unwrap();
+        let implementation_level = seq.next_element()?.unwrap();
+        Ok(FileDescription {
+            description,
+            implementation_level,
+        })
+    }
+
+    // Entry point for Record or Parameter::Typed
+    fn visit_map<A>(self, mut map: A) -> ::std::result::Result<Self::Value, A::Error>
+    where
+        A: ::serde::de::MapAccess<'de>,
+    {
+        let key: String = map
+            .next_key()?
+            .expect("Empty map cannot be accepted as ruststep Holder"); // this must be a bug, not runtime error
+        if key != "FILE_DESCRIPTION" {
+            todo!("Create Error type and send it")
+        }
+        let value = map.next_value()?; // send to Self::visit_seq
+        Ok(value)
     }
 }
 
@@ -28,11 +69,62 @@ pub struct FileName {
 }
 
 impl<'de> de::Deserialize<'de> for FileName {
-    fn deserialize<D>(_deserializer: D) -> ::std::result::Result<Self, D::Error>
+    fn deserialize<D>(deserializer: D) -> ::std::result::Result<Self, D::Error>
     where
         D: de::Deserializer<'de>,
     {
-        todo!()
+        deserializer.deserialize_tuple_struct("FILE_NAME", 7, FileNameVisitor {})
+    }
+}
+
+struct FileNameVisitor;
+
+impl<'de> ::serde::de::Visitor<'de> for FileNameVisitor {
+    type Value = FileName;
+    fn expecting(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(formatter, "FILE_NAME")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> ::std::result::Result<Self::Value, A::Error>
+    where
+        A: ::serde::de::SeqAccess<'de>,
+    {
+        if let Some(size) = seq.size_hint() {
+            if size != 7 {
+                todo!("Create another error and send it")
+            }
+        }
+        let name = seq.next_element()?.unwrap();
+        let time_stamp = seq.next_element()?.unwrap();
+        let author = seq.next_element()?.unwrap();
+        let organization = seq.next_element()?.unwrap();
+        let preprocessor_version = seq.next_element()?.unwrap();
+        let originating_system = seq.next_element()?.unwrap();
+        let authorization = seq.next_element()?.unwrap();
+        Ok(FileName {
+            name,
+            time_stamp,
+            author,
+            organization,
+            preprocessor_version,
+            originating_system,
+            authorization,
+        })
+    }
+
+    // Entry point for Record or Parameter::Typed
+    fn visit_map<A>(self, mut map: A) -> ::std::result::Result<Self::Value, A::Error>
+    where
+        A: ::serde::de::MapAccess<'de>,
+    {
+        let key: String = map
+            .next_key()?
+            .expect("Empty map cannot be accepted as ruststep Holder"); // this must be a bug, not runtime error
+        if key != "FILE_NAME" {
+            todo!("Create Error type and send it")
+        }
+        let value = map.next_value()?; // send to Self::visit_seq
+        Ok(value)
     }
 }
 
@@ -42,11 +134,48 @@ pub struct FileSchema {
 }
 
 impl<'de> de::Deserialize<'de> for FileSchema {
-    fn deserialize<D>(_deserializer: D) -> ::std::result::Result<Self, D::Error>
+    fn deserialize<D>(deserializer: D) -> ::std::result::Result<Self, D::Error>
     where
         D: de::Deserializer<'de>,
     {
-        todo!()
+        deserializer.deserialize_tuple_struct("FILE_SCHEMA", 1, FileSchemaVisitor {})
+    }
+}
+
+struct FileSchemaVisitor;
+
+impl<'de> ::serde::de::Visitor<'de> for FileSchemaVisitor {
+    type Value = FileSchema;
+    fn expecting(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(formatter, "FILE_SCHEMA")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> ::std::result::Result<Self::Value, A::Error>
+    where
+        A: ::serde::de::SeqAccess<'de>,
+    {
+        if let Some(size) = seq.size_hint() {
+            if size != 1 {
+                todo!("Create another error and send it")
+            }
+        }
+        let schema = seq.next_element()?.unwrap();
+        Ok(FileSchema { schema })
+    }
+
+    // Entry point for Record or Parameter::Typed
+    fn visit_map<A>(self, mut map: A) -> ::std::result::Result<Self::Value, A::Error>
+    where
+        A: ::serde::de::MapAccess<'de>,
+    {
+        let key: String = map
+            .next_key()?
+            .expect("Empty map cannot be accepted as ruststep Holder"); // this must be a bug, not runtime error
+        if key != "FILE_SCHEMA" {
+            todo!("Create Error type and send it")
+        }
+        let value = map.next_value()?; // send to Self::visit_seq
+        Ok(value)
     }
 }
 
