9b881c25eaaa41a0979defda2c4e84d78155a19a
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-09-09 00:22:36 +0900

    Add test case for Vec<T>

--- ruststep-derive/tests/try_build.rs
+++ ruststep-derive/tests/try_build.rs
@@ -3,4 +3,5 @@ fn try_build() {
     let t = trybuild::TestCases::new();
     t.pass("tests/cases/use_place_holder.rs");
     t.pass("tests/cases/optional.rs");
+    t.pass("tests/cases/vec.rs");
 }
