d11cf4d48367d70f7b3c47070a1274dfdfb7ce17
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-09-09 00:31:41 +0900

    Add vec.stderr

--- ruststep-derive/tests/try_build.rs
+++ ruststep-derive/tests/try_build.rs
@@ -3,5 +3,6 @@ fn try_build() {
     let t = trybuild::TestCases::new();
     t.pass("tests/cases/use_place_holder.rs");
     t.pass("tests/cases/optional.rs");
-    t.pass("tests/cases/vec.rs");
+    // not implemented yet
+    t.compile_fail("tests/cases/vec.rs");
 }
