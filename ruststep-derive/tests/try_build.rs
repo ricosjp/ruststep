37fa102f89f89f7090d5b05728d570fbf43ec1f5
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-09-09 20:52:19 +0900

    Add codegen for EXPRESS's ARRAY

--- ruststep-derive/tests/try_build.rs
+++ ruststep-derive/tests/try_build.rs
@@ -3,6 +3,5 @@ fn try_build() {
     let t = trybuild::TestCases::new();
     t.pass("tests/cases/use_place_holder.rs");
     t.pass("tests/cases/optional.rs");
-    // not implemented yet
-    t.compile_fail("tests/cases/vec.rs");
+    t.pass("tests/cases/vec.rs");
 }
