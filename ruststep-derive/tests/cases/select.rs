285954df83570a8f6858083c8509740296b91bb5
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-09-12 07:43:20 +0900

    Add test for select

---
+++ ruststep-derive/tests/cases/select.rs
@@ -0,0 +1,33 @@
+use ruststep_derive::{as_holder, Holder};
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct Table {
+    a: HashMap<u64, as_holder!(A)>,
+    b: HashMap<u64, as_holder!(B)>,
+}
+
+#[derive(Debug, Clone, PartialEq, Holder)]
+#[holder(table = Table)]
+#[holder(field = a)]
+pub struct A {
+    pub x: f64,
+}
+
+#[derive(Debug, Clone, PartialEq, Holder)]
+#[holder(table = Table)]
+#[holder(field = b)]
+pub struct B {
+    pub y: f64,
+}
+
+#[derive(Debug, Clone, PartialEq, Holder)]
+#[holder(table = Table)]
+pub enum S {
+    #[holder(field = a)]
+    A(A),
+    #[holder(field = b)]
+    B(B),
+}
+
+fn main() {}
