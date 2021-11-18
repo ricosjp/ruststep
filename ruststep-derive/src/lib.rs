c03a7d1a1bbb2266b87faaf98a9c9f3b21f52484
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-11-18 15:14:49 +0900

    Drop duplicated doc-test

--- ruststep-derive/src/lib.rs
+++ ruststep-derive/src/lib.rs
@@ -133,36 +133,6 @@ fn derive_deserialize(ast: &syn::DeriveInput) -> TokenStream2 {
 ///   - This can be both in field or variant attribute
 ///   - Specify the field is not a simple type
 ///
-/// Holder for struct
-/// ------------------
-///
-/// ```
-/// use ruststep_derive::{as_holder, Holder};
-/// use std::collections::HashMap;
-///
-/// pub struct Table {
-///     a: HashMap<u64, as_holder!(A)>,
-///     b: HashMap<u64, as_holder!(B)>,
-/// }
-///
-/// #[derive(Debug, Clone, PartialEq, Holder)]
-/// #[holder(table = Table)]
-/// #[holder(field = a)]
-/// pub struct A {
-///     pub x: f64,
-///     pub y: f64,
-/// }
-///
-/// #[derive(Debug, Clone, PartialEq, Holder)]
-/// #[holder(table = Table)]
-/// #[holder(field = b)]
-/// pub struct B {
-///     pub z: f64,
-///     #[holder(use_place_holder)]
-///     pub a: A,
-/// }
-/// ```
-///
 #[proc_macro_error]
 #[proc_macro_derive(Holder, attributes(holder))]
 pub fn derive_holder_entry(input: TokenStream) -> TokenStream {
