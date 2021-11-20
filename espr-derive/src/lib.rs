3535493769361240c9ee853f40b7eee736e81fef
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-11-20 15:44:34 +0900

    Add test

--- espr-derive/src/lib.rs
+++ espr-derive/src/lib.rs
@@ -3,6 +3,20 @@ use proc_macro2::TokenStream as TokenStream2;
 use proc_macro_error::{abort_call_site, proc_macro_error};
 use quote::quote;
 
+/// Compile and expand results inline EXPRESS
+///
+/// Example
+/// --------
+///
+/// ```
+/// espr_derive::inline_express!(r#"
+/// SCHEMA explicit_draughting;
+///   ENTITY a;
+///     x: REAL;
+///   END_ENTITY;
+/// END_SCHEMA;
+/// "#);
+/// ```
 #[proc_macro_error]
 #[proc_macro]
 pub fn inline_express(input: TokenStream) -> TokenStream {
