d5be9373093246f6ea11b11e127249a6b7085ac0
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-11-23 23:00:59 +0900

    Use anyhow

--- espr/tests/try_generate.rs
+++ espr/tests/try_generate.rs
@@ -1,5 +1,6 @@
 use espr::{ast::SyntaxTree, codegen::rust::*, ir::IR};
 
+use anyhow::*;
 use std::{
     fs,
     io::Write,
@@ -7,46 +8,44 @@ use std::{
     process::{Command, Stdio},
 };
 
-fn rustfmt(tt: String) -> String {
+fn rustfmt(tt: String) -> Result<String> {
     let mut child = Command::new("rustfmt")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to spawn rustfmt process");
+        .spawn()?;
     let mut stdin = child.stdin.take().expect("Failed to open stdin");
     std::thread::spawn(move || {
         stdin
             .write_all(tt.as_bytes())
             .expect("Failed to write to stdin");
     });
-    let output = child.wait_with_output().expect("Failed to read stdout");
-    String::from_utf8(output.stdout).expect("Non UTF-8 string found")
+    let output = child.wait_with_output()?;
+    Ok(String::from_utf8(output.stdout)?)
 }
 
-fn diff(target: &Path, answer: &Path) {
-    let st = Command::new("diff")
-        .args([target, answer])
-        .status()
-        .expect("Failed to start diff command");
+fn diff(target: &Path, answer: &Path) -> Result<()> {
+    let st = Command::new("diff").args([target, answer]).status()?;
     if !st.success() {
-        panic!("Target and answer are different");
+        bail!("Target and answer are different");
     }
+    Ok(())
 }
 
-fn test(exp: &Path, rs: &Path) {
-    let input = fs::read_to_string(exp).unwrap();
+fn test(exp: &Path, rs: &Path) -> Result<()> {
+    let input = fs::read_to_string(exp)?;
 
     let st = SyntaxTree::parse(&input).expect("Tokenize failed");
-    let ir = IR::from_syntax_tree(&st).expect("Failed in semantic analysis phase");
+    let ir = IR::from_syntax_tree(&st)?;
     let tt = ir.to_token_stream(CratePrefix::External).to_string();
 
-    let tt = rustfmt(tt);
+    let tt = rustfmt(tt)?;
 
     let mut generated = PathBuf::from(rs);
     generated.set_extension("generated.rs");
     fs::write(&generated, tt).unwrap();
 
-    diff(&generated, rs);
+    diff(&generated, rs)?;
+    Ok(())
 }
 
 #[test]
@@ -56,5 +55,6 @@ fn try_generate() {
     test(
         &test_case_root.join("simple.exp"),
         &test_case_root.join("simple.rs"),
-    );
+    )
+    .unwrap();
 }
