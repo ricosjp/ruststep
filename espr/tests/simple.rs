6217e37263f52d4500f5a7261ec2a0c61224654b
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-11-24 01:23:08 +0900

    Move rustfmt to espr::codegen::rust::

--- espr/tests/simple.rs
+++ espr/tests/simple.rs
@@ -1,28 +1,5 @@
 use espr::{ast::SyntaxTree, codegen::rust::*, ir::IR};
 
-use anyhow::*;
-use std::{
-    io::Write,
-    process::{Command, Stdio},
-};
-
-fn rustfmt(tt: String) -> Result<String> {
-    let mut child = Command::new("rustfmt")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()?;
-    // Write input from another thread for avoiding deadlock.
-    // See https://doc.rust-lang.org/std/process/index.html#handling-io
-    let mut stdin = child.stdin.take().expect("Failed to open stdin");
-    std::thread::spawn(move || {
-        stdin
-            .write_all(tt.as_bytes())
-            .expect("Failed to write to stdin");
-    });
-    let output = child.wait_with_output()?;
-    Ok(String::from_utf8(output.stdout)?)
-}
-
 #[test]
 fn simple() {
     let input = r#"
@@ -44,7 +21,7 @@ fn simple() {
     let ir = IR::from_syntax_tree(&st).unwrap();
     let tt = ir.to_token_stream(CratePrefix::External).to_string();
 
-    let tt = rustfmt(tt).unwrap();
+    let tt = rustfmt(tt);
 
     insta::assert_snapshot!(tt, @r###"
     pub mod test_schema {
