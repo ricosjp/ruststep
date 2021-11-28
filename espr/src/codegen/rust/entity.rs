6998d858939436a9ae99100fa8610c55ac83014a
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-11-29 08:14:28 +0900

    Use ToTokens for EntityAttribute while ToTokens for Entity

--- espr/src/codegen/rust/entity.rs
+++ espr/src/codegen/rust/entity.rs
@@ -22,7 +22,7 @@ impl ToTokens for EntityAttribute {
 
         tokens.append_all(quote! {
             #use_place_holder
-            pub #attr_name : #attr_type,
+            pub #attr_name : #attr_type
         });
     }
 }
@@ -31,25 +31,10 @@ impl ToTokens for Entity {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let field_name = format_ident!("{}", self.name);
         let name = format_ident!("{}", self.name.to_pascal_case());
+        let attributes = &self.attributes;
 
         let mut attr_name = Vec::new();
         let mut attr_type = Vec::new();
-        let mut use_place_holder = Vec::new();
-
-        for EntityAttribute { name, ty, optional } in &self.attributes {
-            let name = format_ident!("{}", name);
-            attr_name.push(name.clone());
-            if *optional {
-                attr_type.push(quote! { Option<#ty> });
-            } else {
-                attr_type.push(quote! { #ty });
-            }
-            if ty.is_simple() {
-                use_place_holder.push(quote! {});
-            } else {
-                use_place_holder.push(quote! { #[holder(use_place_holder)] });
-            }
-        }
 
         for ty in &self.supertypes {
             let (attr, ty) = match ty {
@@ -87,7 +72,6 @@ impl ToTokens for Entity {
         }
 
         assert_eq!(attr_name.len(), attr_type.len());
-        assert_eq!(attr_name.len(), use_place_holder.len());
 
         tokens.append_all(quote! {
             #[derive(Debug, Clone, PartialEq, ::derive_new::new, Holder)]
@@ -95,10 +79,8 @@ impl ToTokens for Entity {
             #[holder(field = #field_name)]
             #[holder(generate_deserialize)]
             pub struct #name {
-                #(
-                #use_place_holder
-                pub #attr_name : #attr_type,
-                )*
+                #(pub #attr_name : #attr_type),*
+                #(#attributes),*
             }
         });
 
