298e413966ea82dd7f0d08d4dc72c6f21c064954
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-01-20 19:25:04 +0900

    Set onClick

--- step-playground/src/lib.rs
+++ step-playground/src/lib.rs
@@ -1,20 +1,23 @@
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace=console)]
-    fn log(s: &str);
-}
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::HtmlElement;
 
 #[wasm_bindgen(start)]
 pub fn run() {
-    log("Hello, STEP!");
-}
+    let compile = Closure::wrap(Box::new(move || {
+        let window = web_sys::window().unwrap();
+        let document = window.document().unwrap();
+        let p = document.get_element_by_id("syntax_tree").unwrap();
+        p.set_inner_html("From closure!!");
+    }) as Box<dyn FnMut()>);
 
-#[wasm_bindgen]
-pub fn compile() {
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
-    let p = document.get_element_by_id("syntax_tree").unwrap();
-    p.set_inner_html("Rewrite from wasm");
+    document
+        .get_element_by_id("button")
+        .unwrap()
+        .dyn_ref::<HtmlElement>()
+        .unwrap()
+        .set_onclick(Some(compile.as_ref().unchecked_ref()));
+
+    compile.forget();
 }
