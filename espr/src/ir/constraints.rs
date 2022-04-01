efc16168b6c897f29174b10211458bed432f917b
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-04-01 15:46:42 +0900

    Rewrite Constraints::new by gather_constraint_expr

--- espr/src/ir/constraints.rs
+++ espr/src/ir/constraints.rs
@@ -83,6 +83,12 @@ impl ConstraintExpr {
             ),
         })
     }
+
+    /// Evaluate the expresion into a list of instantiables
+    pub fn as_instantiables(&self, ns: &Namespace) -> Result<Vec<Vec<Path>>, SemanticError> {
+        let is = Instantiables::from_constraint_expr(ns, self)?;
+        Ok(is.as_path(ns))
+    }
 }
 
 /// Global constraints in EXPRESS components
@@ -217,115 +223,13 @@ pub fn gather_constraint_expr(
 
 impl Constraints {
     pub fn new(ns: &Namespace, st: &SyntaxTree) -> Result<Self, SemanticError> {
-        let mut instantiables = HashMap::new();
-        let root = Scope::root();
-        for schema in &st.schemas {
-            let scope = root.schema(&schema.name);
-
-            // Be sure that `SUPERTYPE OF` declaration with complex constraint
-            // using `ONEOF`, `AND` and `ANDOR` are deprecated:
-            //
-            // ISO-10303-11 (2004, en) Page 56, Note 1
-            // > In order that existing schemas remain valid,
-            // > the declaration of subtype/supertype constraints
-            // > that use the keywords ONEOF, ANDOR, or AND within
-            // > the declaration of an entity, as described in this sub-clause,
-            // > remains valid under this edition 2 of EXPRESS.
-            // > However, its use is deprecated, and its removal is planned
-            // > in future editions. The use of the subtype constraint (see 9.7)
-            // > is encouraged instead.
-            //
-            for entity in &schema.entities {
-                match &entity.constraint {
-                    Some(ast::Constraint::SuperTypeRule(expr)) => {
-                        let path = Path::new(&scope, ScopeType::Entity, &entity.name);
-                        instantiables.insert(path, Instantiables::from_expr(ns, &scope, expr)?);
-                    }
-                    _ => continue,
-                }
-            }
-
-            for constraint in &schema.subtype_constraints {
-                if let Some(expr) = &constraint.expr {
-                    let (path, _index) = ns.resolve(&scope, &constraint.entity)?;
-                    let is = Instantiables::from_expr(ns, &scope, expr)?;
-                    match instantiables.entry(path) {
-                        Entry::Occupied(mut e) => {
-                            // ISO-10303-11 (2004, en) Annex B.3 "Interpreting the possible complex entity data types"
-                            // > Combine the subtype expressions sxi of these constraints
-                            // > into a single subtype constraint sti of the form:
-                            // > (sx1 ANDOR sx2 ANDOR sx3 . . . ANDOR sxk ).
-                            e.insert(Instantiables::andor(vec![e.get().clone(), is]));
-                        }
-                        Entry::Vacant(e) => {
-                            e.insert(is);
-                        }
-                    }
-                }
-            }
-        }
-
-        // Add default constraints
-        //
-        // ISO-10303-11 (2004, en) 9.2.5.6 Default constraint between subtypes
-        // > If no supertype constraint is mentioned in the declaration of an entity,
-        // > the subtypes (if any) shall be mutually inclusive, that is,
-        // > as if all subtypes were implicitly mentioned in an ANDOR construct.
-        //
-        let mut super_to_sub = HashMap::new();
-        for schema in &st.schemas {
-            let scope = root.schema(&schema.name);
-            for entity in &schema.entities {
-                let sub = Path::entity(&scope, &entity.name);
-                let (_ast, index) = ns.get(&sub)?;
-                if let Some(subtype_decl) = &entity.subtype_of {
-                    for sup_name in &subtype_decl.entity_references {
-                        let (sup, _) = ns.resolve(&scope, sup_name)?;
-                        let indices: &mut Vec<usize> = super_to_sub.entry(sup).or_default();
-                        indices.push(index);
-                    }
-                }
-            }
-        }
-        // Apply ANDOR to gathered indices
-        for (sup, indices) in super_to_sub {
-            let mut is: Vec<_> = indices
+        let exprs = gather_constraint_expr(ns, st)?;
+        Ok(Constraints {
+            instantiables: exprs
                 .into_iter()
-                .map(|index| Instantiables::single(index))
-                .collect();
-            match instantiables.entry(sup) {
-                Entry::Occupied(mut e) => {
-                    is.push(e.get().clone());
-                    e.insert(Instantiables::andor(is));
-                }
-                Entry::Vacant(e) => {
-                    e.insert(Instantiables::andor(is));
-                }
-            }
-        }
-
-        // Replace indices to Path using Namespace
-        let instantiables = instantiables
-            .into_iter()
-            .map(|(path, it)| {
-                (
-                    path,
-                    it.parts
-                        .iter()
-                        .map(|pce| {
-                            pce.indices
-                                .iter()
-                                .map(|index| {
-                                    let (path, _ast) = &ns[*index];
-                                    path.clone()
-                                })
-                                .collect()
-                        })
-                        .collect(),
-                )
-            })
-            .collect();
-        Ok(Constraints { instantiables })
+                .map(|(path, expr)| Ok((path, expr.as_instantiables(ns)?)))
+                .collect::<Result<_, SemanticError>>()?,
+        })
     }
 
     pub fn is_supertype(&self, path: &Path) -> bool {
